128f894a89e9ba857969e7528bf58c9d65eec0ed
Dinko <dinko.dugec@gmail.com>
2023-01-24 21:37:22 +0100

    showing products in cart...

--- shopping/cart.php
+++ shopping/cart.php
@@ -40,20 +40,30 @@
                 </thead>
                 <tbody>
                   <?php   foreach($allProducts as $product) : ?>
+
                   <tr class="mb-4">
+
                     <th scope="row"><?= $product->product_id; ?></th>
+
                     <td><img width="100" height="100" src="../images/<?php echo $product->product_image; ?>"
                         class="img-fluid rounded-3" alt="Cotton T-shirt">
                     </td>
+
                     <td><?= $product->product_name; ?></td>
-                    <td><?= $product->product_price; ?>€</td>
+
+                    <td class="pro_price"><?= $product->product_price; ?></td>
+
                     <td><input id="form1" min="1" name="quantity" value="<?= $product->product_amount; ?>" type="number"
-                        class="form-control form-control-sm" /></td>
-                    <td>€</td>
+                        class="form-control form-control-sm pro_amount" /></td>
+
+                    <td class="total_price"></td>
+
                     <td><button class="btn btn-warning text-white"><i class="fas fa-pen"></i> </button>
                     </td>
+
                     <td><button class="btn btn-danger text-white"><i class="fas fa-trash-alt"></i> </button>
                     </td>
+
                   </tr>
                   <?php endforeach; ?>
 
@@ -72,7 +82,7 @@
 
               <div class="d-flex justify-content-between mb-5">
                 <h5 class="text-uppercase">Total price</h5>
-                <h5>€ 137.00</h5>
+                <h5 class="full_price"></h5>
               </div>
 
               <button type="button" class="btn btn-dark btn-block btn-lg" data-mdb-ripple-color="dark">Checkout</button>
@@ -88,40 +98,72 @@
 </div>
 </div>
 
-<footer class="bg-dark text-white text-center text-lg-start" style="margin-top: 40px">
-  <!-- Grid container -->
-  <div class="container p-4">
-    <!--Grid row-->
-    <div class="row">
-      <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer Content</h5>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-          voluptatem veniam, est atque cumque eum delectus sint!
-        </p>
-      </div>
-      <!--Grid column-->
 
-      <!--Grid column-->
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Links</h5>
+<?php require "../includes/footer.php"; ?>
 
-        <ul class="list-unstyled mb-0">
-          <li>
-            <a href="#!" class="text-white">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 4</a>
-          </li>
-        </ul>
+<script>
+$(document).ready(function() {
 
-        <?php require "../includes/footer.php"; ?>
+
+  $(".pro_amount").mouseup(function() {
+
+    var $el = $(this).closest('tr');
+
+
+
+    var pro_amount = $el.find(".pro_amount").val();
+    var product_price = $el.find(".pro_price").html();
+
+    var total = pro_amount * product_price;
+    $el.find(".total_price").html("");
+
+    $el.find(".total_price").append(total + '€');
+
+    /*            $(".btn-update").on('click', function(e) {
+
+                 var id = $(this).val();
+
+
+                 $.ajax({
+                   type: "POST",
+                   url: "update-item.php",
+                   data: {
+                     update: "update",
+                     id: id,
+                     product_amount: pro_amount
+                   },
+
+                   success: function() {
+                     // alert("done");
+                     reload();
+                   }
+                 })
+               }); */
+
+
+    fetch();
+  });
+
+  /* fetch(); */
+
+  function fetch() {
+
+    setInterval(function() {
+      var sum = 0.0;
+      $('.total_price').each(function() {
+        sum += parseFloat($(this).text());
+      });
+      $(".full_price").html(sum + "$");
+    }, 4000); // 4 seconds
+  }
+
+  /*     function reload() {
+
+
+        $("body").load("cart.php")
+
+      } */
+
+});
+</script>
