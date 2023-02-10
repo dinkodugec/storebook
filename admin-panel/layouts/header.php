aacc1ec78e0bf2383242d097769ed49183b59a45
Dinko <dinko.dugec@gmail.com>
2023-02-11 00:29:56 +0100

    creating index page for admins and dynamic show number of products, admins and categories

--- admin-panel/layouts/header.php
+++ admin-panel/layouts/header.php
@@ -30,7 +30,7 @@
           <?php  if(isset($_SESSION['adminname'])) : ?>
           <ul class="navbar-nav side-nav">
             <li class="nav-item">
-              <a class="nav-link text-white" style="margin-left: 20px;" href="index.php">Home
+              <a class="nav-link text-white" style="margin-left: 20px;" href="<?php echo ADMINURL; ?>">Home
                 <span class="sr-only">(current)</span>
               </a>
             </li>
