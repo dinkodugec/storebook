2ae6a67689ddf2006ec7e638abad7bfd4aeb5a31
Dinko <dinko.dugec@gmail.com>
2023-02-12 20:46:48 +0100

    finis update

--- admin-panel/categories-admins/update-category.php
+++ admin-panel/categories-admins/update-category.php
@@ -11,6 +11,39 @@
 
       $categories = $select->fetch(PDO::FETCH_OBJ);
 
+
+      if(isset($_POST['submit'])){
+        if(empty($_POST['name'] OR empty($_POST['description']))){
+          echo "<script>alert('One or more inputs are empty')</script>";
+          
+      } else {
+        unlink("images/".$categories->image."");
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $image= $_FILES['image']['name'];
+
+        $dir = "images/" . basename($image);
+
+        $update = $conn->prepare("UPDATE categories SET name=:name, description=:description, name=:image WHERE id='$id'"); 
+                              
+        $update->execute([
+          ":name" => $name,
+           ":description" => $description,
+           ":image" => $image
+        ]);
+
+        if(move_uploaded_file($_FILES['image']['tmp_name'], $dir)){
+           header("Location: ".ADMINURL."/categories-admins/show-categories.php"); 
+
+            
+          
+        }
+      }
+   
+    }
+
+      
+
      } else {
 
           header("Location: http://store.hr:8080/404.php");
@@ -24,7 +57,7 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title mb-5 d-inline">Update Categories</h5>
-        <form method="POST" action="" enctype="multipart/form-data">
+        <form method="POST" action="update-category.php?id=<?php echo $id; ?>" enctype="multipart/form-data">
           <!-- Email input -->
           <div class="form-outline mb-4 mt-4">
             <input type="text" name="name" value="<?php echo $categories->name; ?>" id="form2Example1"
